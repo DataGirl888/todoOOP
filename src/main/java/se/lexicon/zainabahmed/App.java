fe6dca074363cea8ab62d6f0611124ae82fb1e97
DataGirl888 <datadrivenproductivity@gmail.com>
2020-07-09 19:19:21 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/zainabahmed/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.zainabahmed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
